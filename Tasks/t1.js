e1580ce6ea56219298263aee31892ebee99d6ef3
Karam Abbas <karamabbas03@gmail.com>
2025-03-30 17:39:32 +0500

    testing

--- Tasks/t1.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var a = 123;
-console.log(a);
-function Hellow() {
-    console.log("hellow");
-}
--- Tasks/t1.ts
+++ Tasks/t1.ts
@@ -1,5 +1,2 @@
 var a = 123;
 console.log(a);
-function Hellow( ){
-console.log("hellow");
-}
